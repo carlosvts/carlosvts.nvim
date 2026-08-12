dcb0efb7d5922cb665bf9726fdc96531ad2316e1
carlosvts <carlosvtsdev@gmail.com>
2026-08-12 16:27:55 -0300

    feat: add 4 spaces instead of 2

--- lua/carlosvts/options.lua
+++ lua/carlosvts/options.lua
@@ -33,9 +33,9 @@ function M.setup()
   opt.breakindent = true
   opt.linebreak = true
   opt.expandtab = true
-  opt.shiftwidth = 2
-  opt.tabstop = 2
-  opt.softtabstop = 2
+  opt.shiftwidth = 4
+  opt.tabstop = 4
+  opt.softtabstop = 4
   opt.smartindent = true
   opt.completeopt = { 'menu', 'menuone', 'noselect' }
   opt.shortmess:append 'I'
